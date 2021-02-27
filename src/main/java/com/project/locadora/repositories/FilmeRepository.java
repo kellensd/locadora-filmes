d4f0b010c938629eda8a3a5371ce53815cdd29ff
kellensd <kellensd@hotmail.com>
2021-02-27 18:52:01 -0300

    Criação das repositories

---
+++ src/main/java/com/project/locadora/repositories/FilmeRepository.java
@@ -0,0 +1,9 @@
+package com.project.locadora.repositories;
+
+import com.project.locadora.model.Filme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
+}
---
+++ src/main/java/com/project/locadora/repositories/LocacaoRepository.java
@@ -0,0 +1,9 @@
+package com.project.locadora.repositories;
+
+import com.project.locadora.model.Locacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocacaoRepository extends JpaRepository<Locacao, Long> {
+}
