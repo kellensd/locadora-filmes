f0355a2e1ea173ae0dcbb124e7844f0c472cac21
kellensd <kellensd@hotmail.com>
2021-02-28 13:04:11 -0300

    criado a classe usuarioDTO

---
+++ src/main/java/com/project/locadora/dto/UsuarioDTO.java
@@ -0,0 +1,23 @@
+package com.project.locadora.dto;
+
+public class UsuarioDTO {
+
+    private String email;
+    private String nomeCompleto;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+}
