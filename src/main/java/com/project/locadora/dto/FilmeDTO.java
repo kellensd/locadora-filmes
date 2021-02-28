1d9ab66ba19800595acdabe6a76737a4aa021870
kellensd <kellensd@hotmail.com>
2021-02-27 23:17:09 -0300

    Criado a classe filmeDTO

---
+++ src/main/java/com/project/locadora/dto/FilmeDTO.java
@@ -0,0 +1,24 @@
+package com.project.locadora.dto;
+
+public class FilmeDTO {
+
+    private String titulo;
+
+    private String diretor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
