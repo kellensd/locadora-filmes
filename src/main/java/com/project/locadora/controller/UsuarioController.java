ea98d488fc385f6f6e2afe596efa667211034d2a
kellensd <kellensd@hotmail.com>
2021-02-28 13:28:54 -0300

    Adicionado novo endpoint de pesquisa de usuario por id

--- src/main/java/com/project/locadora/controller/UsuarioController.java
+++ src/main/java/com/project/locadora/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.project.locadora.controller;
 
+import com.project.locadora.dto.UsuarioDTO;
 import com.project.locadora.model.Usuario;
 import com.project.locadora.representation.UsuarioIn;
 import com.project.locadora.service.impl.UsuarioServiceImpl;
@@ -7,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -24,8 +26,11 @@ public class UsuarioController {
 
     private UsuarioServiceImpl usuarioService;
 
-    public UsuarioController(UsuarioServiceImpl usuarioService) {
+    private ModelMapper modelMapper;
+
+    public UsuarioController(UsuarioServiceImpl usuarioService, ModelMapper modelMapper) {
         this.usuarioService = usuarioService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
@@ -59,6 +64,27 @@ public class UsuarioController {
         if (usuario.isPresent()) {
             usuarioService.delete(usuario.get());
             return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping(value = "/{id}")
+    @ApiOperation(value = "Pesquisa de usuário pelo id.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Usuário retornado com sucesso.", response = UsuarioDTO.class),
+                    @ApiResponse(code = 404, message = "Usuário não encontrado."),
+                    @ApiResponse(code = 500, message = "Ocorreu um erro inesperado no servidor ao pesquisar o usuário.")
+            }
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<UsuarioDTO> findByTitulo(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.findById(id);
+
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.map(usuario1 -> modelMapper.map(usuario, UsuarioDTO.class))
+                            .get());
         } else {
             return ResponseEntity.notFound().build();
         }
