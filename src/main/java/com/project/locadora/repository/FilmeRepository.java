f04f8cc1c75f64672816023e6eb7856050b44904
kellensd <kellensd@hotmail.com>
2021-02-28 02:02:59 -0300

    Criado nova consulta, na FilmeRepository, para buscar filmes de quantidade maior que zero

--- src/main/java/com/project/locadora/repository/FilmeRepository.java
+++ src/main/java/com/project/locadora/repository/FilmeRepository.java
@@ -4,10 +4,13 @@ import com.project.locadora.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
 
-    Optional<Filme> findByTitulo(String titulo);
+    Optional<Filme> findByTituloIgnoreCase(String titulo);
+
+    List<Filme> findByQuantidadeGreaterThan(int quantidade);
 }
