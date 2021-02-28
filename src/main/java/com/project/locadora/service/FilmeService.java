aaa41610f80830565d30d2eb15f594310133de0e
kellensd <kellensd@hotmail.com>
2021-02-28 02:04:41 -0300

    Criado novos métodos de findFilmesDisponiveis e locarFilme na service

--- src/main/java/com/project/locadora/service/FilmeService.java
+++ src/main/java/com/project/locadora/service/FilmeService.java
@@ -1,11 +1,16 @@
 package com.project.locadora.service;
 
 import com.project.locadora.dto.FilmeDTO;
+import com.project.locadora.exception.FilmeIndisponivelException;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FilmeService {
 
     Optional<FilmeDTO> findByTitulo(String titulo);
 
+    List<FilmeDTO> findFilmesDisponiveis();
+
+    void locarFilme(String titulo) throws FilmeIndisponivelException;
 }
