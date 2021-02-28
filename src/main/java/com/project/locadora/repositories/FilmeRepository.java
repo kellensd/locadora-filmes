6572d361a9a684976a8aa45cd49c70e3c81c6540
kellensd <kellensd@hotmail.com>
2021-02-27 22:34:53 -0300

    Criado consulta por titulo na FilmeRepository

--- src/main/java/com/project/locadora/repositories/FilmeRepository.java
+++ src/main/java/com/project/locadora/repositories/FilmeRepository.java
@@ -4,6 +4,10 @@ import com.project.locadora.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+
+    Optional<Filme> findByTitulo(String titulo);
 }
