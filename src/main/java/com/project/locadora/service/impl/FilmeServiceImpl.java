90eb746d69165de339eaac4dbea5d009d7c8ef0d
kellensd <kellensd@hotmail.com>
2021-02-28 02:07:23 -0300

    Na classe de implementações da service:  - foi ajustado o método findByTitulo para consultar o titulo do filme com ignore case.  - criado novo método de consultar filmes disponiveis.  - criado novo método para locar filme.

--- src/main/java/com/project/locadora/service/impl/FilmeServiceImpl.java
+++ src/main/java/com/project/locadora/service/impl/FilmeServiceImpl.java
@@ -1,12 +1,16 @@
 package com.project.locadora.service.impl;
 
 import com.project.locadora.dto.FilmeDTO;
+import com.project.locadora.exception.FilmeIndisponivelException;
+import com.project.locadora.model.Filme;
 import com.project.locadora.repository.FilmeRepository;
 import com.project.locadora.service.FilmeService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FilmeServiceImpl implements FilmeService {
@@ -22,7 +26,26 @@ public class FilmeServiceImpl implements FilmeService {
 
     @Override
     public Optional<FilmeDTO> findByTitulo(String titulo) {
-        return filmeRepository.findByTitulo(titulo)
+        return filmeRepository.findByTituloIgnoreCase(titulo)
                 .map(filme -> modelMapper.map(filme, FilmeDTO.class));
+    }
+
+    @Override
+    public List<FilmeDTO> findFilmesDisponiveis() {
+        return filmeRepository.findByQuantidadeGreaterThan(0)
+                .stream()
+                .map(filme -> modelMapper.map(filme, FilmeDTO.class))
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public void locarFilme(String titulo) throws FilmeIndisponivelException {
+        Filme filme = filmeRepository.findByTituloIgnoreCase(titulo)
+                .filter(filme1 -> filme1.getQuantidade() > 0)
+                .orElseThrow(() -> new FilmeIndisponivelException("Filme indisponível para alugar!"));
+
+        filme.locarFilme();
+        filmeRepository.save(filme);
     }
 }
