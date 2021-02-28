7e798a3020014ac5db1c1e3d5ea65d0adb6360f8
kellensd <kellensd@hotmail.com>
2021-02-28 02:20:33 -0300

    Criado novos métodos de devolverFilme na service

--- src/main/java/com/project/locadora/service/FilmeService.java
+++ src/main/java/com/project/locadora/service/FilmeService.java
@@ -2,6 +2,7 @@ package com.project.locadora.service;
 
 import com.project.locadora.dto.FilmeDTO;
 import com.project.locadora.exception.FilmeIndisponivelException;
+import com.project.locadora.exception.FilmeInexistenteException;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,6 @@ public interface FilmeService {
     List<FilmeDTO> findFilmesDisponiveis();
 
     void locarFilme(String titulo) throws FilmeIndisponivelException;
+
+    void devolverFilme(String titulo) throws FilmeInexistenteException;
 }
