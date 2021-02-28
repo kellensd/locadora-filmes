e57ae3e8891bc92234ced76e3e1623649ce04698
kellensd <kellensd@hotmail.com>
2021-02-28 02:21:54 -0300

    Criado a exception FilmeInexistenteException nos casos em que for devolver um filme que não foi encontrado na base

---
+++ src/main/java/com/project/locadora/exception/FilmeInexistenteException.java
@@ -0,0 +1,8 @@
+package com.project.locadora.exception;
+
+public class FilmeInexistenteException extends Exception {
+
+    public FilmeInexistenteException(String message) {
+        super(message);
+    }
+}
