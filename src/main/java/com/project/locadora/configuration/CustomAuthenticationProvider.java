7d750edc1f72925607c9bef72e18e846b99cc7c3
kellensd <kellensd@hotmail.com>
2021-02-28 23:06:13 -0300

    Criado classes de configurações de segurança

---
+++ src/main/java/com/project/locadora/configuration/CustomAuthenticationProvider.java
@@ -0,0 +1,54 @@
+package com.project.locadora.configuration;
+
+import com.project.locadora.business.Criptografia;
+import com.project.locadora.model.Usuario;
+import com.project.locadora.repository.UsuarioRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Objects;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private UsuarioRepository usuarioRepository;
+
+    public CustomAuthenticationProvider(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+        final UsernamePasswordAuthenticationToken autenticacao = (UsernamePasswordAuthenticationToken) authentication;
+        final String name = (String) authentication.getPrincipal();
+
+        final String password = (String) autenticacao.getCredentials();
+
+        String encodedPassword = Criptografia.getHashMd5(password);
+
+        final String storedPassword = usuarioRepository.findByNomeIgnoreCase(name).map(Usuario::getSenha)
+                .orElseThrow(() -> new IllegalArgumentException("illegal id or password"));
+
+        if (Objects.equals(encodedPassword, "") || !Objects.equals(encodedPassword, storedPassword)) {
+            throw new AuthenticationServiceException("Unauthorized");
+        }
+
+        final Object principal = authentication.getPrincipal();
+        final UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(
+                principal, authentication.getCredentials(),
+                Collections.emptyList());
+        result.setDetails(authentication.getDetails());
+
+        return result;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
---
+++ src/main/java/com/project/locadora/configuration/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.project.locadora.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    public WebSecurityConfig(CustomAuthenticationProvider customAuthenticationProvider) {
+        this.customAuthenticationProvider = customAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity
+                    .authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                    .authorizeRequests().antMatchers("/console/**").permitAll()
+                .and()
+                    .authorizeRequests()
+                        .antMatchers(HttpMethod.GET, "/usuarios").authenticated()
+                        .antMatchers(HttpMethod.DELETE, "/usuarios").authenticated()
+                .and()
+                    .authorizeRequests()
+                        .antMatchers(HttpMethod.POST, "/filmes").authenticated()
+                        .antMatchers(HttpMethod.DELETE, "/filmes").authenticated()
+                        .antMatchers(HttpMethod.PATCH, "/filmes").authenticated()
+                .and()
+                    .httpBasic();
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+    protected void configure(AuthenticationManagerBuilder authBuilder) {
+            authBuilder.authenticationProvider(customAuthenticationProvider);
+    }
+}
